c0832db76a5d37b50165db48ca21399e01ec6341
ducky <duckyabso.y24@gmail.com>
2024-12-03 10:01:57 +0700

    update

--- project_source/routes/web.php
+++ project_source/routes/web.php
@@ -62,5 +62,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
-require __DIR__ . '/api/building-room.php';
+require __DIR__ . '/api/building-room-residence.php';
 require __DIR__ . '/api/notification.php';

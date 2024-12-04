b5bcb80152c921ac722625cb77e882f615676150
vyminh <vyvominh@gmail.com>
2024-12-04 13:32:09 +0700

    Del api

--- project_source/routes/api/report_accountant.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\ReportAccountantController;
-
-Route::middleware('auth')->group(function () {
-    Route::get('/report_accountant', [ReportAccountantController::class, 'index'])->name('report_accountant.index');
-});
